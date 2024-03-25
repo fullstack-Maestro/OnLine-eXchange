4acb9fca59cdd69f504ba0020ea820b81ab0567e
Diyorbek <muhammadjonovd793@gmail.com>
2024-03-25 16:15:26 +0500

    Added Seed Data

--- Olx.Domain/Configurations/Constants.cs
+++ Olx.Domain/Configurations/Constants.cs
@@ -2,5 +2,5 @@
 
 public static class Constants
 {
-    public const string ConnectionString = "Host=localhost;Port=5432;Username=postgres;Password=0511;Database=OnLine-eXchange-db";
+    public const string ConnectionString = "Server=rain.db.elephantsql.com;Database=yjwqieym;User Id=yjwqieym;Password=vuRr8PnBvcLwvPqNWtX8gojFF33s6KkW";
 }
